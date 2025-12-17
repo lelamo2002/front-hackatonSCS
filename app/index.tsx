0ec796da05c640774d17438d8de10bfd4b312abb
Leonardo Lago Moreno <leleolagomoreno@gmail.com>
2025-12-17 15:33:17 -0300

    feat: setup nativeWind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View
+      className="bg-red-500"
+    >
+      <Text>Edit app/index.tsx to edit bla this screen.</Text>
+    </View>
+  );
+}
