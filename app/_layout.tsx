0618c3a5fe82f761a4288980e015632b1399620c
Leonardo Lago Moreno <leleolagomoreno@gmail.com>
2025-12-17 16:34:16 -0300

    feat: adicionando safeArea

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
 import { Stack } from "expo-router";
+import { View } from "react-native";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const insets = useSafeAreaInsets();
+  return (
+  <View className="flex-1" style={{ paddingTop: insets.top }}>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  </View>
+  );
 }
