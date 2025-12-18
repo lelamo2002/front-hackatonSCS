ef8431b1f399d13a832554035d595477c002558b
VitorB2002 <vitorb.santos2002@gmail.com>
2025-12-18 10:26:00 -0300

    feat: removendo mapa da tela inicial

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,12 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import "../global.css";
-import OSMWebView from "./components/OSMWebView";
 
 export default function Index() {
 
   return (
-    {/* -- MAPA NO FUNDO (Ocupa tudo) -- */}
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-bold mb-6">SCS Hackathon</Text>
-      <OSMWebView />
       <View className="space-y-4">
         <Link href="/login" asChild>
           <Text className="text-blue-500 text-lg p-2">Ir para Login</Text>
