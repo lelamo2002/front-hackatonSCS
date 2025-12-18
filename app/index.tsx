8f64f640ac1dad0fae31036a58e8ff9fe41699bc
vitor <vitorb.santos2002@gmail.com>
2025-12-17 21:29:52 -0300

    feat: adicionado map via webview

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,17 @@
-import { Button } from "@react-navigation/elements";
 import { useRouter } from "expo-router"; // 1. Importar o hook de rotas
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import "../global.css";
+import OSMWebView from "./components/OSMWebView";
 
 export default function Index() {
   const router = useRouter(); // 2. Inicializar o hook
 
   return (
-    <View
-      className="bg-red-500"
-    >
-      <Text>Edit app/index.tsx to edit bla this screen.</Text>
-      <Button 
-        onPress={() => router.push("/park/check-in")} // 3. Ação de navegar
-      >
-        Ir para Check-in
-      </Button>
+    <View className="flex-1 bg-white">
+      {/* -- MAPA NO FUNDO (Ocupa tudo) -- */}
+      <View className="absolute inset-0 z-0">
+        <OSMWebView />
+      </View>
     </View>
   );
 }
